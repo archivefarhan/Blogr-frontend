ce6e183cd2aafe744055e8918d4c5fbb5c37eb5d
farhan <farhanj.mohamed@gmail.com>
2023-02-20 17:06:01 -0800

    blog formatting in progress

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,7 +1,76 @@
 export function Blog() {
   return (
     <div className="w-screen h-auto min-h-screen">
-      <p className="mt-10 text-center font-black text-4xl">Blog</p>
+      <p className="mt-10 text-center font-black text-4xl">Blog Home</p>
+      <br />
+      <div className="grid grid-cols-3 grid-rows-3 mx-auto bg-sky-400 gap-1 p-5">
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+            <div>
+              <a href="">Home</a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+        <div className="bg-black text-white mx-auto mt-5">
+          <div className="border border-black text-center">
+            <p>title</p>
+            <p>img</p>
+            <p>description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
