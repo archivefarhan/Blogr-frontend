d9105258bf4727b9c97681ce729f750c2d114973
farhan <farhanj.mohamed@gmail.com>
2023-02-20 17:19:04 -0800

    blog and header updated. reformatting in progress

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,14 +3,14 @@ export function Blog() {
     <div className="w-screen h-auto min-h-screen">
       <p className="mt-10 text-center font-black text-4xl">Blog Home</p>
       <br />
-      <div className="grid grid-cols-3 grid-rows-3 mx-auto bg-sky-400 gap-1 p-5">
-        <div className="bg-black text-white mx-auto mt-5">
+      <div className="mt-96 grid grid-cols-3 grid-rows-3 mx-auto bg-sky-400 gap-1 p-5">
+        <div className="bg-green-600 text-white mx-auto mt-5">
           <div className="border border-black text-center">
-            <p>title</p>
             <p>img</p>
+            <p>title</p>
             <p>description</p>
-            <div>
-              <a href="">Home</a>
+            <div className="rounded-xl w-36 bg-sky-400 text-center">
+              <a href="">Read Full Article</a>
             </div>
           </div>
         </div>
